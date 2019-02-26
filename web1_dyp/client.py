6bb506d43eb4b0b96afe0a0b02cd4d4a1817af3d
dyp <duan.yapeng@meteodyn.com>
2019-02-26 17:09:07 +0800

    a

--- web1_dyp/client.py
+++ web1_dyp/client.py
@@ -13,7 +13,11 @@ ip, port = s.getsockname()
 print('本机ip 和 port {} {}'.format(ip, port))
 # 本地的端口是操作系統分配的，本地的ip是路由器分配的
 
+#构造一个HTTP请求
 http_request = 'GET / HTTP/1.1\r\nhost:{}\r\n\r\n'.format(host)
+# 发送HTTP 请求给服务器
+# send函数只接受bytes作为参数
+# str.encode把str转换为bytes，编码是utf-8
 
 request= http_request.encode('utf-8')
 print('请求', request)
