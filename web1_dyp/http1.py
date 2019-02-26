bdefb57a54d9569ab922240a91e627ce40bbd61f
dyp <duan.yapeng@meteodyn.com>
2019-02-26 18:06:59 +0800

    a

--- web1_dyp/http1.py
+++ web1_dyp/http1.py
@@ -7,9 +7,14 @@ s = socket.socket()
 
 s.bind((host, port))
 
+# bind连接一个tuple,客户端程序要用connect, 服务器程序要用bind
+
 while True:
     s.listen(5)
+    # 当有客户端过来连接的时候，s.accept函数就会返回2个值
+    # 分别是 连接 和客户 ip 地址
     connection, address = s.accept()
+    print('after accept 类型', type(connection), type(address))
 
     request = connection.recv(1024)
     print('ip and request, {}\n{}'.format(address, request.decode('utf-8')))
