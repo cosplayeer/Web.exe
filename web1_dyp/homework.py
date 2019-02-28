ca95431adee67511c51125ec6be1e7845fe00c81
dyp <duan.yapeng@meteodyn.com>
2019-02-28 11:05:17 +0800

    parsed_url rewrite

--- web1_dyp/homework.py
+++ web1_dyp/homework.py
@@ -14,8 +14,8 @@ def protocol_of_url(url):
     'http://g.cn/'
 
     返回代表协议的字符串, 'http' 或者 'https'
-    '''
-    pass
+       '''
+    
 
 def protocol_of_url_only(url):
     protocol = 'http'
@@ -25,9 +25,42 @@ def protocol_of_url_only(url):
         protocol = 'https'
         u = url.split('://')[1]
     else:
-        u = url   #protocal value ?
+        u = url   
 
-    return protocal 
+    return protocol 
+
+def parsed_url(url):
+    protocol = 'http'
+    if url[:7] == 'http://':
+        protocol = url[:7]
+        u = url.split('://')[1]
+    elif url[:8] =='https://':
+        protocol = url[:8]
+        u = url.split('://')[1]
+    else:
+        u = url
+    
+    if '/' not in u:
+        host = u
+        path = '/'
+    else:
+        host = u.split('/')[0]
+        path = u.split('/')[1]
+
+    port_dict = {
+        'http':80,
+        'https':443,
+    }
+    port = port_dict[protocol]
+    if ':' not in host:
+        host = host
+        port = port
+    else:
+        h = host.split(':')
+        host = h[0]
+        port = h[1]
+    
+    return protocol, host, port, path
 
 def protocol_of_url(url):
     protocol = 'http'
@@ -37,7 +70,7 @@ def protocol_of_url(url):
         protocol = 'https'
         u = url.split('://')[1]
     else:
-        u = url   #protocal value ?
+        u = url  
 
     i = u.find('/')
     if i == -1:
